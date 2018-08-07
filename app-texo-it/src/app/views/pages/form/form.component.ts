ea25fa8c22f5de87d5a2ed0fa6d3a4960fd3b253
Ivam Galvão Filho <ivam.developer@gmail.com>
2018-08-06 22:34:07 -0300

    Format_Date01

--- app-texo-it/src/app/views/pages/form/form.component.ts
+++ app-texo-it/src/app/views/pages/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 import { MenuModule } from 'primeng/menu';
 import { MenuItem } from 'primeng/api';
 
@@ -10,17 +11,29 @@ import { MenuItem } from 'primeng/api';
 export class FormComponent implements OnInit {
 val1: string;
 val2: string;
-valid: Date;
-fabr: Date;
+public valid: Date;
+public fabr: Date;
+public now: any;
 money: number;
+public dataValid: any;
 public perecivel = false;
   constructor() { }
 
-  ngOnInit() {}
-dataValidade(event) {
+  ngOnInit() {
+    let now = moment().format('DD/MM/YYYY');
+    console.log(now);     
+  }
+  dataValidade(event) {
     this.perecivel = false;
     if (event.target.autocomplete === 'on') {
       this.perecivel = true;
-    }
+    }     
   }
+  dataVenc() {
+    this.dataValid = this.valid.toLocaleDateString();
+    this.now = moment().format('DD/MM/YYYY');
+    console.log(this.valid.toLocaleDateString());
+    console.log(this.now);
+    if ( this.dataValid < this.now ) {return console.log('Produto vencido!');}
+  } 
 }
